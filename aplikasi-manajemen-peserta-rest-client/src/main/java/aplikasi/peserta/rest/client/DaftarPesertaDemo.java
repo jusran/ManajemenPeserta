ff2ce4c42e27529c1a4302485cfa79a5cb55431d
yoez <uze_run@yahoo.com>
2011-09-30 10:32:39 +0700

    tambah project rest client

---
+++ aplikasi-manajemen-peserta-rest-client/src/main/java/aplikasi/peserta/rest/client/DaftarPesertaDemo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplikasi.peserta.rest.client;
+
+import org.springframework.web.client.RestTemplate;
+
+/**
+ *
+ * @author Student-08
+ */
+public class DaftarPesertaDemo {
+    public static void main(String[] args) {
+//        RestTemplate 
+    }
+}
